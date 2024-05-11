352b99a8124433c715448b8c4f603f1737aaf716
adib <adibbaihaqi11@gmail.com>
2024-05-11 21:43:23 +0800

    change something not important

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -3,7 +3,7 @@
     <form method="POST" action="/">
         <div class="mb-3">
             @csrf
-            <label for="exampleInputEmail1" class="form-label">Task Description</label>
+            <label for="Task" class="form-label">Task Description</label>
             <input type="text" class="form-control" name="description">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
